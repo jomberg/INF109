891df99467b503788ce36294eefd8ea331727253
Jan Thomas Moldung <jan.thomas.moldung@evry.com>
2014-08-27 19:12:01 +0200

    lære løkker, prettyprinting...

--- jtm-2gangen-ver3.py
+++ jtm-2gangen-ver3.py
@@ -1,8 +1,8 @@
 
 def multiply(base, max):
-    for i in range(1, max):
+    for i in range(1, max+1):
         result = base * i
-        print ("%2d * %2d = %2d") % (base, i, result)
+        print ("%2d * %2d = %2d" %(base, i, result))
 
 def main():
     # Skriv ut 2-gangen
@@ -10,3 +10,9 @@ def main():
     multiply(2, max)
 
 main()
+
+# str1 = a
+# str2 = b
+# int1 = 100
+# print ("nr 1: " + str1 + " nr2: " + str2)
+# print ("int1: " + str(int1))
